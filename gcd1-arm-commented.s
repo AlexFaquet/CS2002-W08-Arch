d2f671c0c604a36f088b8e4d7d902dcf64a8ad68
AlexFaquet <alexfaquet2003@gmail.com>
2026-03-23 22:16:02 +0000

    Finished commenting all files

--- gcd1-arm-commented.s
+++ gcd1-arm-commented.s
@@ -5,14 +5,14 @@
 	.type	gcd,@function
 gcd:
 	.cfi_startproc
-	stp	x29, x30, [sp, #-16]!
-	mov	x29, sp
+	stp	x29, x30, [sp, #-16]!	// save frame pointer (x29, equivalent to %rbp in x86) and return address (x30) to stack, decrement sp by 16 (prologue)
+	mov	x29, sp					// set frame pointer x29 to current stack pointer - establishes stack frame (prologue)
 	.cfi_def_cfa w29, 16
 	.cfi_offset w30, -8
 	.cfi_offset w29, -16
-	bl	gcd_recurse
-	ldp	x29, x30, [sp], #16
-	ret
+	bl	gcd_recurse				// call gcd_recurse(a, b) - saves return address in x30 and jumps to gcd_recurse
+	ldp	x29, x30, [sp], #16		// restore frame pointer (x29) and return address (x30) from stack, increment sp by 16 (epilogue)
+	ret							// return to caller - jumps to address stored in link register x30
 .Lfunc_end0:
 	.size	gcd, .Lfunc_end0-gcd
 	.cfi_endproc
@@ -21,23 +21,23 @@ gcd:
 	.type	gcd_recurse,@function
 gcd_recurse:
 	.cfi_startproc
-	cbz	w1, .LBB1_2
-	stp	x29, x30, [sp, #-16]!
-	mov	x29, sp
+	cbz	w1, .LBB1_2				// if b (w1) is zero, jump to base case at .LBB1_2 - otherwise, carry on with prologue
+	stp	x29, x30, [sp, #-16]!	// b is not zero - save frame pointer and return address before recursive call overwrites x30 (prologue)
+	mov	x29, sp					// set frame pointer x29 to current stack pointer - establishes stack frame (prologue)
 	.cfi_def_cfa w29, 16
 	.cfi_offset w30, -8
 	.cfi_offset w29, -16
-	sdiv	w8, w0, w1
-	msub	w8, w8, w1, w0
-	mov	w0, w1
-	mov	w1, w8
-	bl	gcd_recurse
-	ldp	x29, x30, [sp], #16
-	ret
+	sdiv	w8, w0, w1			// compute quotient of a / b, store in w8
+	msub	w8, w8, w1, w0		// compute remainder: a - (quotient * b) = a % b, store in w8
+	mov	w0, w1					// set first argument for recursive call - new a = old b
+	mov	w1, w8					// set second argument for recursive call - new b = a % b (remainder from division)
+	bl	gcd_recurse				// call gcd_recurse(b, a % b) recursively - saves return address in x30
+	ldp	x29, x30, [sp], #16		// restore frame pointer and return address from stack, increment sp by 16 (epilogue)
+	ret							// return to caller - jumps to address stored in link register in x30
 .LBB1_2:
-	cmp	w0, #0
-	cneg	w0, w0, mi
-	ret
+	cmp	w0, #0					// compare a (w0) with 0 - sets flag register
+	cneg	w0, w0, mi			// if a is negative (mi = minus), negate w0 to get absolute value
+	ret							// return to caller - absolute value of result is in w0
 .Lfunc_end1:
 	.size	gcd_recurse, .Lfunc_end1-gcd_recurse
 	.cfi_endproc
--- gcd2-x86-commented.s
+++ gcd2-x86-commented.s
@@ -5,33 +5,34 @@
 	.type	gcd,@function
 gcd:
 	.cfi_startproc
-	pushq	%rbp
+	pushq	%rbp				# save caller's base pointer onto the stack (prologue)
 	.cfi_def_cfa_offset 16
 	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
+	movq	%rsp, %rbp			# set base pointer to current stack pointer (prologue)
 	.cfi_def_cfa_register %rbp
-	movl	%edi, %eax
-	testl	%esi, %esi
-	je	.LBB0_1
-	movl	%esi, %edx
+	movl	%edi, %eax			# copy a into %eax - preserves a for later use
+	testl	%esi, %esi			# test if b is zero by ANDing %esi with itself, 
+								# can only be zero if every single bit in the result is 0 - sets flag register
+	je	.LBB0_1					# if b is zero, jump to base case at .LBB0_1
+	movl	%esi, %edx			# copy b into %edx - %edx will track current b through the loop
 	.p2align	4, 0x90
 .LBB0_3:
-	movl	%edx, %ecx
-	cltd
-	idivl	%ecx
-	movl	%ecx, %eax
-	testl	%edx, %edx
-	jne	.LBB0_3
-	jmp	.LBB0_4
+	movl	%edx, %ecx			# copy b into %ecx to preserve it before cltd overwrites %edx
+	cltd						# sign extend %eax into %edx:%eax to prepare for idivl
+	idivl	%ecx				# divide a by b - quotient goes into %eax, remainder goes into %edx
+	movl	%ecx, %eax			# new a = old b (update for next iteration)
+	testl	%edx, %edx			# test if remainder (new b) is zero - sets flag register
+	jne	.LBB0_3					# if remainder is not zero, loop back and repeat
+	jmp	.LBB0_4					# if remainder is zero - algorithm complete, jump to end
 .LBB0_1:
-	movl	%eax, %ecx
+	movl	%eax, %ecx			# b was zero from the start - copy a into %ecx to preserve it before negl overwrites it
 .LBB0_4:
-	movl	%ecx, %eax
-	negl	%eax
-	cmovsl	%ecx, %eax
-	popq	%rbp
+	movl	%ecx, %eax			# load result into %eax ready for absolute value check
+	negl	%eax				# negate %eax - also sets sign flag as side effect
+	cmovsl	%ecx, %eax			# if sign flag set (result was positive), restore original value from %ecx
+	popq	%rbp				# restore caller's base pointer (epilogue)
 	.cfi_def_cfa %rsp, 8
-	retq
+	retq						# return to caller - absolute value of result is in %eax
 .Lfunc_end0:
 	.size	gcd, .Lfunc_end0-gcd
 	.cfi_endproc
